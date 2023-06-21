3303cb7d079fec732e2f8e8443030b951551a524
Praveen880890 <76040957+Praveen880890@users.noreply.github.com>
2023-06-21 12:57:07 +0530

    added some functionality

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 from img2table.document import Image
 from img2table.document import PDF
-from IPython.display import display_html
+##from IPython.display import display_html
 from img2table.ocr import TesseractOCR
 import img2pdf
 from PIL import Image
@@ -9,19 +9,19 @@ from img2table.ocr import EasyOCR
 
 easyocr = EasyOCR(lang=["en"])
 
-img_path = "tt.jpg"
-pdf_path = "ff.pdf"
-image = Image.open(img_path)
-pdf_bytes = img2pdf.convert(image.filename)
-file = open(pdf_path, "wb")
-file.write(pdf_bytes)
-image.close()
-file.close()
-print("Successfully made pdf file")
+##img_path = "tt.jpg"
+##pdf_path = "ff.pdf"
+##image = Image.open(img_path)
+##pdf_bytes = img2pdf.convert(image.filename)
+##file = open(pdf_path, "wb")
+##file.write(pdf_bytes)
+##image.close()
+##file.close()
+##print("Successfully made pdf file")
 
-tesseract_ocr = TesseractOCR(n_threads=1, lang="eng")
+##tesseract_ocr = TesseractOCR(n_threads=1, lang="eng")
 # Instantiation of the image
-pdf = PDF(src="ff.pdf")
+pdf = PDF(src="timetable.pdf")
 
 # Extract tables
 pdf.to_xlsx('tables.xlsx',
